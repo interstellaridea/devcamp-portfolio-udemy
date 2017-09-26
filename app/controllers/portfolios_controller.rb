3e235c763633c2afcd963a5873744e2525b270ef
Sebastian Valdez <valdez.sebastian4@gmail.com>
2017-09-25 19:03:47 -0700

    Integrated cocoon for portfolio items nested form component

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -20,7 +20,6 @@ class PortfoliosController < ApplicationController
 
   def new
     @portfolio_item = Portfolio.new
-    3.times { @portfolio_item.technologies.build}
   end
 
   def create
@@ -39,7 +38,7 @@ class PortfoliosController < ApplicationController
 
   def update
     respond_to do |format|
-      if @portfolio_item.update(params.require(:portfolio).permit(:title,:subtitle,:body))
+      if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
       else
         format.html { render :edit }
@@ -63,13 +62,12 @@ class PortfoliosController < ApplicationController
   private
 
     def portfolio_params
-      params.require(:portfolio).permit(:title,
-                                        :subtitle,
-                                        :body,
-                                        :main_image,
-                                        :thumb_image,
-                                        technologies_attributes: [:name]
-                                        )
+        params.require(:portfolio).permit(:title,
+                                          :subtitle,
+                                          :body,
+                                          :main_image,
+                                          :thumb_image,
+                                          technologies_attributes: [:id, :name, :_destroy])
     end
 
     def set_portfolio_item
