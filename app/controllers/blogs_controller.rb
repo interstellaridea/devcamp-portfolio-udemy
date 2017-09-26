45da7aa82cce14b9fba60518cefc68fa7583b312
Sebastian Valdez <valdez.sebastian4@gmail.com>
2017-09-25 20:05:51 -0700

    Refactored alerts into helper method and set topic to optional in blogs model

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -33,11 +33,9 @@ class BlogsController < ApplicationController
 
     respond_to do |format|
       if @blog.save
-        format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
-        format.json { render :show, status: :created, location: @blog }
+        format.html { redirect_to @blog, notice: 'Your post is now live.' }
       else
         format.html { render :new }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
       end
     end
   end
