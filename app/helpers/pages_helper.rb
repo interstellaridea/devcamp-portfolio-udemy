23f4d00f8d487249113cf65c55b115aecf2717ae
Sebastian Valdez <valdez.sebastian4@gmail.com>
2017-09-26 20:30:04 -0700

    Added pages helper method twitter parse to return clickable links

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,24 @@
 module PagesHelper
+
+	def twitter_parser tweet
+		regex = %r{
+      \b
+      (
+        (?: [a-z][\w-]+:
+         (?: /{1,3} | [a-z0-9%] ) |
+          www\d{0,3}[.] |
+          [a-z0-9.\-]+[.][a-z]{2,4}/
+        )
+        (?:
+         [^\s()<>]+ | \(([^\s()<>]+|(\([^\s()<>]+\)))*\)
+        )+
+        (?:
+          \(([^\s()<>]+|(\([^\s()<>]+\)))*\) |
+          [^\s`!()\[\]{};:'".,<>?«»“”‘’]
+        )
+      )
+    }ix
+
+		tweet.gsub(regex){|link| "<a href='#{link}' target='_blank'>#{link}</a>"}.html_safe
+	end
 end
